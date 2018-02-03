963c255a3dd4c71f816dcf9f6c359240dda9f5c9
Colten <coltenbadgley@gmail.com>
2018-02-02 21:33:52 -0800

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Num7 = New System.Windows.Forms.Button()
+        Me.Num8 = New System.Windows.Forms.Button()
+        Me.Num9 = New System.Windows.Forms.Button()
+        Me.Num6 = New System.Windows.Forms.Button()
+        Me.Num5 = New System.Windows.Forms.Button()
+        Me.Num4 = New System.Windows.Forms.Button()
+        Me.Num3 = New System.Windows.Forms.Button()
+        Me.Num2 = New System.Windows.Forms.Button()
+        Me.Num1 = New System.Windows.Forms.Button()
+        Me.OpEquals = New System.Windows.Forms.Button()
+        Me.Num0 = New System.Windows.Forms.Button()
+        Me.NumDot = New System.Windows.Forms.Button()
+        Me.OpPlus = New System.Windows.Forms.Button()
+        Me.OpMinus = New System.Windows.Forms.Button()
+        Me.OpMultiply = New System.Windows.Forms.Button()
+        Me.OpDivide = New System.Windows.Forms.Button()
+        Me.OpClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(2, -4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(332, 35)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Num7
+        '
+        Me.Num7.BackColor = System.Drawing.Color.Transparent
+        Me.Num7.FlatAppearance.BorderSize = 0
+        Me.Num7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num7.Location = New System.Drawing.Point(7, 119)
+        Me.Num7.Name = "Num7"
+        Me.Num7.Size = New System.Drawing.Size(105, 44)
+        Me.Num7.TabIndex = 1
+        Me.Num7.UseVisualStyleBackColor = False
+        '
+        'Num8
+        '
+        Me.Num8.BackColor = System.Drawing.Color.Transparent
+        Me.Num8.FlatAppearance.BorderSize = 0
+        Me.Num8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num8.Location = New System.Drawing.Point(114, 119)
+        Me.Num8.Name = "Num8"
+        Me.Num8.Size = New System.Drawing.Size(110, 43)
+        Me.Num8.TabIndex = 2
+        Me.Num8.UseVisualStyleBackColor = False
+        '
+        'Num9
+        '
+        Me.Num9.BackColor = System.Drawing.Color.Transparent
+        Me.Num9.FlatAppearance.BorderSize = 0
+        Me.Num9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num9.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num9.Location = New System.Drawing.Point(223, 119)
+        Me.Num9.Name = "Num9"
+        Me.Num9.Size = New System.Drawing.Size(110, 43)
+        Me.Num9.TabIndex = 3
+        Me.Num9.UseVisualStyleBackColor = False
+        '
+        'Num6
+        '
+        Me.Num6.BackColor = System.Drawing.Color.Transparent
+        Me.Num6.FlatAppearance.BorderSize = 0
+        Me.Num6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num6.Location = New System.Drawing.Point(223, 76)
+        Me.Num6.Name = "Num6"
+        Me.Num6.Size = New System.Drawing.Size(111, 43)
+        Me.Num6.TabIndex = 6
+        Me.Num6.UseVisualStyleBackColor = False
+        '
+        'Num5
+        '
+        Me.Num5.BackColor = System.Drawing.Color.Transparent
+        Me.Num5.FlatAppearance.BorderSize = 0
+        Me.Num5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num5.Location = New System.Drawing.Point(114, 76)
+        Me.Num5.Name = "Num5"
+        Me.Num5.Size = New System.Drawing.Size(110, 43)
+        Me.Num5.TabIndex = 5
+        Me.Num5.UseVisualStyleBackColor = False
+        '
+        'Num4
+        '
+        Me.Num4.BackColor = System.Drawing.Color.Transparent
+        Me.Num4.FlatAppearance.BorderSize = 0
+        Me.Num4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num4.Location = New System.Drawing.Point(6, 76)
+        Me.Num4.Name = "Num4"
+        Me.Num4.Size = New System.Drawing.Size(106, 43)
+        Me.Num4.TabIndex = 4
+        Me.Num4.UseVisualStyleBackColor = False
+        '
+        'Num3
+        '
+        Me.Num3.BackColor = System.Drawing.Color.Transparent
+        Me.Num3.FlatAppearance.BorderSize = 0
+        Me.Num3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num3.Location = New System.Drawing.Point(223, 30)
+        Me.Num3.Name = "Num3"
+        Me.Num3.Size = New System.Drawing.Size(110, 46)
+        Me.Num3.TabIndex = 9
+        Me.Num3.UseVisualStyleBackColor = False
+        '
+        'Num2
+        '
+        Me.Num2.BackColor = System.Drawing.Color.Transparent
+        Me.Num2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Num2.FlatAppearance.BorderSize = 0
+        Me.Num2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num2.Location = New System.Drawing.Point(114, 30)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(110, 46)
+        Me.Num2.TabIndex = 8
+        Me.Num2.UseVisualStyleBackColor = False
+        '
+        'Num1
+        '
+        Me.Num1.BackColor = System.Drawing.Color.Transparent
+        Me.Num1.FlatAppearance.BorderSize = 0
+        Me.Num1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num1.Location = New System.Drawing.Point(5, 30)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(107, 46)
+        Me.Num1.TabIndex = 7
+        Me.Num1.UseVisualStyleBackColor = False
+        '
+        'OpEquals
+        '
+        Me.OpEquals.BackColor = System.Drawing.Color.Transparent
+        Me.OpEquals.FlatAppearance.BorderSize = 0
+        Me.OpEquals.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpEquals.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpEquals.Location = New System.Drawing.Point(265, 206)
+        Me.OpEquals.Name = "OpEquals"
+        Me.OpEquals.Size = New System.Drawing.Size(68, 45)
+        Me.OpEquals.TabIndex = 12
+        Me.OpEquals.UseVisualStyleBackColor = False
+        '
+        'Num0
+        '
+        Me.Num0.BackColor = System.Drawing.Color.Transparent
+        Me.Num0.FlatAppearance.BorderSize = 0
+        Me.Num0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num0.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Num0.Location = New System.Drawing.Point(114, 164)
+        Me.Num0.Name = "Num0"
+        Me.Num0.Size = New System.Drawing.Size(110, 44)
+        Me.Num0.TabIndex = 11
+        Me.Num0.UseVisualStyleBackColor = False
+        '
+        'NumDot
+        '
+        Me.NumDot.BackColor = System.Drawing.Color.Transparent
+        Me.NumDot.FlatAppearance.BorderSize = 0
+        Me.NumDot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.NumDot.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumDot.Location = New System.Drawing.Point(223, 163)
+        Me.NumDot.Name = "NumDot"
+        Me.NumDot.Size = New System.Drawing.Size(111, 43)
+        Me.NumDot.TabIndex = 10
+        Me.NumDot.UseVisualStyleBackColor = False
+        '
+        'OpPlus
+        '
+        Me.OpPlus.BackColor = System.Drawing.Color.Transparent
+        Me.OpPlus.FlatAppearance.BorderSize = 0
+        Me.OpPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpPlus.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpPlus.Location = New System.Drawing.Point(6, 206)
+        Me.OpPlus.Name = "OpPlus"
+        Me.OpPlus.Size = New System.Drawing.Size(68, 45)
+        Me.OpPlus.TabIndex = 16
+        Me.OpPlus.UseVisualStyleBackColor = False
+        '
+        'OpMinus
+        '
+        Me.OpMinus.BackColor = System.Drawing.Color.Transparent
+        Me.OpMinus.FlatAppearance.BorderSize = 0
+        Me.OpMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpMinus.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpMinus.Location = New System.Drawing.Point(74, 208)
+        Me.OpMinus.Name = "OpMinus"
+        Me.OpMinus.Size = New System.Drawing.Size(61, 44)
+        Me.OpMinus.TabIndex = 15
+        Me.OpMinus.UseVisualStyleBackColor = False
+        '
+        'OpMultiply
+        '
+        Me.OpMultiply.BackColor = System.Drawing.Color.Transparent
+        Me.OpMultiply.FlatAppearance.BorderSize = 0
+        Me.OpMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpMultiply.Location = New System.Drawing.Point(133, 208)
+        Me.OpMultiply.Name = "OpMultiply"
+        Me.OpMultiply.Size = New System.Drawing.Size(65, 43)
+        Me.OpMultiply.TabIndex = 14
+        Me.OpMultiply.UseVisualStyleBackColor = False
+        '
+        'OpDivide
+        '
+        Me.OpDivide.BackColor = System.Drawing.Color.Transparent
+        Me.OpDivide.FlatAppearance.BorderSize = 0
+        Me.OpDivide.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpDivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpDivide.Location = New System.Drawing.Point(199, 208)
+        Me.OpDivide.Name = "OpDivide"
+        Me.OpDivide.Size = New System.Drawing.Size(65, 44)
+        Me.OpDivide.TabIndex = 13
+        Me.OpDivide.UseVisualStyleBackColor = False
+        '
+        'OpClear
+        '
+        Me.OpClear.BackColor = System.Drawing.Color.Transparent
+        Me.OpClear.FlatAppearance.BorderSize = 0
+        Me.OpClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpClear.Location = New System.Drawing.Point(5, 253)
+        Me.OpClear.Name = "OpClear"
+        Me.OpClear.Size = New System.Drawing.Size(328, 42)
+        Me.OpClear.TabIndex = 17
+        Me.OpClear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(340, 302)
+        Me.Controls.Add(Me.OpClear)
+        Me.Controls.Add(Me.OpPlus)
+        Me.Controls.Add(Me.OpMinus)
+        Me.Controls.Add(Me.OpMultiply)
+        Me.Controls.Add(Me.OpDivide)
+        Me.Controls.Add(Me.OpEquals)
+        Me.Controls.Add(Me.Num0)
+        Me.Controls.Add(Me.NumDot)
+        Me.Controls.Add(Me.Num3)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.Num1)
+        Me.Controls.Add(Me.Num6)
+        Me.Controls.Add(Me.Num5)
+        Me.Controls.Add(Me.Num4)
+        Me.Controls.Add(Me.Num9)
+        Me.Controls.Add(Me.Num8)
+        Me.Controls.Add(Me.Num7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Simple Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Num7 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents Num1 As Button
+    Friend WithEvents OpEquals As Button
+    Friend WithEvents Num0 As Button
+    Friend WithEvents NumDot As Button
+    Friend WithEvents OpPlus As Button
+    Friend WithEvents OpMinus As Button
+    Friend WithEvents OpMultiply As Button
+    Friend WithEvents OpDivide As Button
+    Friend WithEvents OpClear As Button
+End Class
